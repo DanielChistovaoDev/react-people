69e9d6aaa0843ddd46bec0a8540d2d2ccb2b8ddb
DanielChistovaoDev <contato.danielchistovao@gmail.com>
2020-06-09 00:11:34 -0300

    tratamento da string, aprendendo pasta util, avatar do user, classe Touchable

--- src/components/PeopleListItem.js
+++ src/components/PeopleListItem.js
@@ -1,13 +1,28 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
-
+import React from 'react';
+import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
+import { capitalizeFirstLetter } from '../util/'
 
 const PeopleListItem =props =>{
 
+    const {people} = props;
+    const {title, first, last} = people.name;
+    const imgUri = people.picture.thumbnail;
+
     return (
-        <View  style={styles.line}>  
-            <Text style={styles.lineText}> {props.people.name.first} </Text>
-        </View>
+        
+        <TouchableOpacity >
+
+            <View  style={styles.line}>
+                <Image style={styles.avatar} source={{uri: imgUri }}/>
+                
+                <Text style={styles.lineText}> 
+                    {`${ capitalizeFirstLetter(title)} `  +
+                     `${ capitalizeFirstLetter(first)} `  +
+                     `${capitalizeFirstLetter(last)}`}
+                </Text>
+            </View>
+
+        </TouchableOpacity>
     );
 }
 
@@ -24,8 +39,15 @@ const styles = StyleSheet.create({
     lineText: {
         fontSize: 20,
         paddingLeft: 20,
-        
+        flex: 7
+    },
+    avatar: {
+        aspectRatio: 1,
+        flex: 1,
+        marginLeft:15,
+        borderRadius: 50
     }
+
 });
 
 export default PeopleListItem;
